d07b6f9a890fa91b6dcdee370a1c5b72cb27fe93
andres192x <andres192x@hotmail.com>
2022-12-07 19:31:37 -0500

    added logic to User.js and userRoutes.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,17 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require("sequelize");
+const { beforeCreate } = require('../config/connection');
 // import our database connection from config.js
 const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class User extends Model {}
+
+
+class User extends Model {
+  checkPassword(loginPw) {
+      return  bcrypt.compareSync(loginPw, this.password);
+  }
+}  
 
 // set up fields and rules for Product model
 User.init(
@@ -15,15 +22,44 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    user_name: {
+    username: {
+      type: DataTypes.STRING,
+      // prevents null values
+      allowNull: false,
+      // will only allow alphanumeric characters
+      validate: {
+        isAlphanumeric: true,
+      },
+    },
+    email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
-    user_password: {
+    password: {
       type: DataTypes.STRING,
       allowNull: false,
+      // must be longer than 8 characters
+      validate: {
+        len: [8],
+      },
     },
+
+
   },
+
+    // user_name: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    // },
+    // user_password: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    // },
+  
   {
     sequelize,
     timestamps: false,
@@ -34,3 +70,5 @@ User.init(
 );
 
 module.exports = User;
+
+
--- signup-login/user.js
+++ signup-login/user.js
@@ -1,54 +1,54 @@
-const { Model, DataTypes } = require('sequelize');
-const { beforeCreate } = require('../config/connection');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const { beforeCreate } = require('../config/connection');
+// const sequelize = require('../config/connection');
 
-class User extends Model {
-    checkPassword(loginPw) {
-        return  bcrypt.compareSync(loginPw, this.password);
-    }
-}  
+// class User extends Model {
+//     checkPassword(loginPw) {
+//         return  bcrypt.compareSync(loginPw, this.password);
+//     }
+// }  
 
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      // prevents null values
-      allowNull: false,
-      // will only allow alphanumeric characters
-      validate: {
-        isAlphanumeric: true,
-      },
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      // must be longer than 8 characters
-      validate: {
-        len: [8],
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
-  }
-);
+// User.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     username: {
+//       type: DataTypes.STRING,
+//       // prevents null values
+//       allowNull: false,
+//       // will only allow alphanumeric characters
+//       validate: {
+//         isAlphanumeric: true,
+//       },
+//     },
+//     email: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       unique: true,
+//       validate: {
+//         isEmail: true,
+//       },
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       // must be longer than 8 characters
+//       validate: {
+//         len: [8],
+//       },
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'user',
+//   }
+// );
 
-module.exports = User;
+// module.exports = User;
