33685e1c48399f20b2a1330490fd1e8c297db522
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2022-12-10 11:44:43 -0500

    Created playlist row and deleted request to retrieve a single song

--- controllers/api/playlistRoutes.js
+++ controllers/api/playlistRoutes.js
@@ -5,14 +5,18 @@ const { Playlist, User, Song } = require("../../models");
 router.get("/:userID", async (req, res) => {
   try {
     const playlistData = await User.findAll({
-    where:{id:req.params.userID},
-      include: [{
-        model:Playlist,
-        include:[{
-          model:Song
-        }]
-      }]
-});
+      where: { id: req.params.userID },
+      include: [
+        {
+          model: Playlist,
+          include: [
+            {
+              model: Song,
+            },
+          ],
+        },
+      ],
+    });
     res.status(200).json(playlistData);
   } catch (err) {
     res.status(500).json(err);
@@ -20,28 +24,19 @@ router.get("/:userID", async (req, res) => {
 });
 
 //create row for playlist
-
-
-router.delete("/:id", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
-    const deletedSongData = await Playlist.destroy({
+    const playlistRow = await Playlist.create(req.body, {
       where: {
-        id: req.params.id,
+        id: req.params.user_id,
+        id: req.params.song_id,
       },
     });
-
-    if (!songData) {
-      res.status(404).json({ message: "No song located with this id!" });
-      return;
-    }
-
-    res.status(200).json(deletedSongData);
+    res.status(200).json(playlistRow);
   } catch (err) {
     res.status(500).json(err);
   }
 });
-//create Playlist row with the user id and the song id
-
 
 
 // GET a single playlist (may need to use findOne?)
@@ -49,7 +44,7 @@ router.delete("/:id", async (req, res) => {
 //   try {
 //     const playlistData = await Playlist.findByPk(req.params.id, {
 //       // JOIN with User, using the Song through table
-//       include: [{ model: User, through: Song, 
+//       include: [{ model: User, through: Song,
 //       /*as: "planned_trips"*/ }],
 //     });
 
--- controllers/api/songRoutes.js
+++ controllers/api/songRoutes.js
@@ -13,23 +13,34 @@ router.get("/", async (req, res) => {
   }
 });
 
-// GET a single song
-// router.get("/:id", async (req, res) => {
-//   try {
-//     const songData = await Song.findByPk(req.params.id);
-
-//     if (!songData) {
-//       res.status(404).json({ message: "No song located with this id!" });
-//       return;
-//     }
-//     res.status(200).json(songData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+// CREATE a song
+router.post("/", async (req, res) => {
+  try {
+    const newSong = await Song.create(req.body);
+    res.status(200).json(newSong);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // DELETE a song by its 'id' value
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedSongData = await Song.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
+    if (!deletedSongData) {
+      res.status(404).json({ message: "No song located with this id!" });
+      return;
+    }
+
+    res.status(200).json(deletedSongData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
-
