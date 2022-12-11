ebcf11dc9c9834fa94c9ba36c25ca776e809b780
Gareth Kwan <gareth0201@gmail.com>
2022-12-11 14:34:07 -0500

    Edited homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,17 @@
 const router = require("express").Router();
 const path = require("path");
+const { Song } = require("../models");
 
 //Create route for the API
 //Gets references for html pages
 router.get("/", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
 router.get("/login", (req, res) => res.sendFile(path.join(__dirname, "../public/loginSignup.html")));
 router.get("/homepage", (req, res) => res.sendFile(path.join(__dirname, "../public/homepage.html")));
-router.get("/video", (req, res) => res.sendFile(path.join(__dirname, "../public/videoPage.html")));
+router.get("/videoPage", (req, res) => res.sendFile(path.join(__dirname, "../public/videoPage.html")));
 router.get("/user", (req, res) => res.sendFile(path.join(__dirname, "../public/userPlaylist.html")));
 router.get("/meditation", (req, res) => res.sendFile(path.join(__dirname, "../public/meditation.html")));
 router.get("/lofi", (req, res) => res.sendFile(path.join(__dirname, "../public/lofi.html")));
+
+router.get("/videoPage/:id", async (req, res) =>res.sendFile(path.join(__dirname, "../public/videoPage.html")));
+
 module.exports = router;
