2a9f61db278571a5a00964d021508d9e9c03fecd
Hpyorange <jaspercheng2022@gmail.com>
2022-12-11 14:59:41 -0500

    change html name and fix connection

--- public/js/meditation.js
+++ public/js/meditation.js
@@ -1,14 +1,7 @@
 // Variable Declaration
-<<<<<<< HEAD
 let row = $('#data')
 
 
-=======
-let songTitle = document.querySelector(".title");
-let category = document.querySelector(".singer");
-let videoContainer = document.querySelector(".videoContainer");
-let row = $("#data");
->>>>>>> ebcf11dc9c9834fa94c9ba36c25ca776e809b780
 
 function runSong() {
   let musicAPI = "http://localhost:3001/api/songs";
@@ -18,12 +11,12 @@ function runSong() {
     })
     .then(function (data) {
       console.log(data);
-      data.forEach((element) => {
+      for(let i=0; i<4; i++) {
         row.append(`
          <tr>      
            <td>
              <div>
-               <p class="song">${element.song_name}</p>
+               <p class="song">${data[i].song_name}</p>
              </div>
            </td>
         
@@ -33,55 +26,14 @@ function runSong() {
                <i class="fa-solid fa-circle-plus icon" id="add"></i>
              </div>
            </td>
+        
          </tr>`);
-<<<<<<< HEAD
       }
     });
 
-=======
-      });
-    });
->>>>>>> ebcf11dc9c9834fa94c9ba36c25ca776e809b780
 }
 
 //the buttons are going to be href that will send a home route request /videopage/1
 //home request is going to serve the videoPage.html
 
 runSong();
-
-<<<<<<< HEAD
-=======
-//---------------Gareth's Work-------------------
-// console.log(data);
-//       songs = [0, 1, 2, 3, 4, 5, 6, 7];
-//       songs.forEach(function (i) {
-//         let tableRow = document.createElement("tr");
-//         let tableSongName = document.createElement("td");
-//         let tableSongCategory = document.createElement("td");
-//         tableSongName.textContent = data[i].song_name;
-//         tableSongCategory.textContent = data[i].category;
-//         tableSongName.setAttribute("class", "tableSongName");
-//         tableSongCategory.setAttribute("class", "tableSongCategory");
-//         // document.getElementsByClassName("tableSongName").style.fontWeight = "500";
-//         // document.getElementsByClassName("tableSongCategory").style.fontWeight = 900;
-//         songList.appendChild(tableRow);
-//         tableRow.appendChild(tableSongName);
-//         tableRow.appendChild(tableSongCategory);
-//       });
-
-// function runMusicAPI() {
-//   fetch(musicAPI)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data.song_source);
-//       let video = document.createElement("video");
-//       video.setAttribute("src", "");
-//       let container = document.getElementById("container_1");
-//       container.append(video);
-//     });
-// }
-
-//event.button > invoke runMusicAPI
->>>>>>> ebcf11dc9c9834fa94c9ba36c25ca776e809b780
